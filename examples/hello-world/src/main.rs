54cc663abaf2247ce5ac9dad0c3496a08bad33e4
Dawei Hu <hoodavy@gmail.com>
2025-12-21 22:39:53 +0800

    update

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -71,25 +71,27 @@ async fn main() {
 
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     let listener = TcpListener::bind(addr).unwrap();
+
+    let dummy_middleware = MapResponseLayer::new(|mut res: Response| -> Response {
+        // If there is a content-length header, its value will be zero and axum will avoid
+        // overwriting it. But this means our content-length doesn’t match the length of the
+        // body, which leads to panics in Hyper. Thus we have to ensure that axum doesn’t add
+        // on content-length headers until after middleware has been run.
+        assert!(!res.headers().contains_key("content-length"));
+
+        let mut rng = rand::rng();
+        let mut nums: Vec<i32> = (1..3).collect();
+        // And take a random pick (yes, we didn't need to shuffle first!):
+        let num = nums.choose_mut(&mut rng).unwrap();
+        if (*num == 2) {
+            *res.body_mut() = "hijacked…\n".into();
+        }
+        res
+    });
     let app = Router::new()
         .route("/", get(root))
         .route("/sub", get(sub))
-        .layer(MapResponseLayer::new(|mut res: Response| -> Response {
-            // If there is a content-length header, its value will be zero and axum will avoid
-            // overwriting it. But this means our content-length doesn’t match the length of the
-            // body, which leads to panics in Hyper. Thus we have to ensure that axum doesn’t add
-            // on content-length headers until after middleware has been run.
-            assert!(!res.headers().contains_key("content-length"));
-
-            let mut rng = rand::rng();
-            let mut nums: Vec<i32> = (1..3).collect();
-            // And take a random pick (yes, we didn't need to shuffle first!):
-            let num = nums.choose_mut(&mut rng).unwrap();
-            if (*num == 2) {
-                *res.body_mut() = "hijacked…\n".into();
-            }
-            res
-        }))
+        // .layer(dummy_middleware)
         .with_state(AppState {
             data: "no arc".to_string(),
         });
